b78c259c7bbfae4815c5810a8dc30d19d74c9258
Benjamin R. Haskell <docker@benizi.com>
2017-12-07 14:33:16 -0500

    Allow `$REPO` env var to pick Caddy fork

--- builder/builder.sh
+++ builder/builder.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+: ${REPO=mholt}
 VERSION=${VERSION:-"0.10.11"}
 
 # caddy
-git clone https://github.com/mholt/caddy -b "v$VERSION" /go/src/github.com/mholt/caddy \
-    && cd /go/src/github.com/mholt/caddy \
+git clone https://github.com/$REPO/caddy -b "v$VERSION" /go/src/github.com/$REPO/caddy \
+    && cd /go/src/github.com/$REPO/caddy \
     && git checkout -b "v$VERSION"
 
 # plugin helper
@@ -15,14 +16,14 @@ alias caddyplug='GOOS=linux GOARCH=amd64 caddyplug'
 for plugin in $(echo $PLUGINS | tr "," " "); do \
     go get -v $(caddyplug package $plugin); \
     printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" > \
-        /go/src/github.com/mholt/caddy/caddyhttp/$plugin.go ; \
+        /go/src/github.com/$REPO/caddy/caddyhttp/$plugin.go ; \
     done
 
 # builder dependency
 git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
 
 # build
-cd /go/src/github.com/mholt/caddy/caddy \
+cd /go/src/github.com/$REPO/caddy/caddy \
     && git checkout -f \
     && GOOS=linux GOARCH=amd64 go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM \
     && mkdir -p /install \
