0d5bed49c65f247acaaad8c7361928b074a688f7
Benjamin R. Haskell <docker@benizi.com>
2018-03-22 16:21:00 -0400

    Rewrite builder script to allow other source repos
    Makes selecting upstream repos for mholt/caddy and abiosoft/caddy-git
    much easier, at the cost of hard-coding the list of plugins (removes the
    use of `caddyplug`).

--- builder/builder.sh
+++ builder/builder.sh
@@ -1,31 +1,59 @@
 #!/bin/sh
+#
+# Build `caddy` with `caddy-git` plugin, possibly using alternate source repos.
+#
+# Usage (in docker):
+#
+#   docker run --rm builder-image /build [version]
+#
+# Version to build, including the `v` prefix, is passed either as the first
+# argument or via the `$version` env var (default: `v0.10.11`).  Any valid Git
+# reference should work.
+#
+# Upstream sources for `caddy` and `caddy-git` are set via `$upstream_caddy`
+# (default: `mholt`) and `$upstream_caddy_git` (default: `abiosoft`) env vars.
+# Both can be set using the shorthand `$upstream` env var.
 
-: ${REPO=benizi}
-VERSION=${VERSION:-"0.10.11"}
+set -e
 
-# caddy
-git clone https://github.com/$REPO/caddy -b "v$VERSION" /go/src/github.com/mholt/caddy \
-    && cd /go/src/github.com/mholt/caddy \
-    && git checkout -b "v$VERSION"
+: ${version:=$1}
+: ${version:=v0.10.11}
+: ${upstream_caddy:=$upstream}
+: ${upstream_caddy:=mholt}
+: ${upstream_caddy_git:=$upstream}
+: ${upstream_caddy_git:=abiosoft}
 
-# plugin helper
-GOOS=linux GOARCH=amd64 go get -v github.com/abiosoft/caddyplug/caddyplug
-alias caddyplug='GOOS=linux GOARCH=amd64 caddyplug'
+pkg() { printf 'github.com/%s/%s' "$@" ; }
 
-# plugins
-for plugin in $(echo $PLUGINS | tr "," " "); do \
-    go get -v $(caddyplug package $plugin); \
-    printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" > \
-        /go/src/github.com/mholt/caddy/caddyhttp/$plugin.go ; \
-    done
+gosrc() { printf '/go/src/%s' "$(pkg "$@")" ; }
 
-# builder dependency
-git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
+clone() {
+  local users=$1 repo=$2
+  shift 2
+  local github=${users%%:*} go=${users##*:}
+  git clone --depth 1 https://"$(pkg $github $repo)" "$(gosrc $go $repo)"
+  test -n "$1" || return 0
+  cd "$(gosrc $go $repo)"
+  git checkout --force "$1"
+}
 
-# build
-cd /go/src/github.com/mholt/caddy/caddy \
-    && git checkout -f \
-    && GOOS=linux GOARCH=amd64 go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM \
-    && mkdir -p /install \
-    && mv caddy /install
+{
+  printf 'Building Caddy (version=%s)\n' "$version"
+  printf '  %s src: https://%s\n' \
+    caddy "$(pkg $upstream_caddy caddy)" \
+    caddy-git "$(pkg $upstream_caddy_git caddy-git)"
+} >&2
 
+clone "${upstream_caddy}":mholt caddy $version
+clone "${upstream_caddy_git}":abiosoft caddy-git
+clone caddyserver builds
+
+cat > "$(gosrc mholt caddy)"/caddyhttp/extraplugins.go <<ADD_PKG_GO
+package caddyhttp
+import _ "$(pkg abiosoft caddy-git)"
+ADD_PKG_GO
+
+cd "$(gosrc mholt caddy)"/caddy
+export CGO_ENABLED=0 GOOS=${GOOS:-linux} GOARCH=${GOARCH:-amd64}
+trim="-trimpath=$GOPATH"
+go build -o /install/caddy -asmflags "$trim" -gcflags "$trim"
